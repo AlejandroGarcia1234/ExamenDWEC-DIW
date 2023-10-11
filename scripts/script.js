447f4929c1c43e01de8fdbf1b29813e567dd579c
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2023-10-11 10:29:22 +0200

    Añadimos el comentario del if

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const calculateButton = document.getElementById("calculate");
     const resultContainer = document.getElementById("result");
 
+//Con la siguiente función conseguimos que al hacer click sobre el botón al que le hemos asignado anteriormente el id "calculate"
+//se realicen las operaciones consiguientes, en este caso le pasamos los parámetros de "length" y "width" y al pasar por el if nos
+//mostrará el resultado correspondiente.
 calculateButton.addEventListener("click", () => {
     const length = parseFloat(document.getElementById("length").value);
     const width = parseFloat(document.getElementById("width").value);
-
+//Lo que hará el siguiente if es que si le pasamos los valores de "length" y "width" nos hará las operaciones que le indiquemos,
+//y si no introducimos uno de los dos parametros, o ninguno de ellos, nos saltará el mensaje especificado en el else.
     if ( !isNaN(length) && !isNaN(width)){
         const perimetro = 2 * length + width;
         const area = length * width;
